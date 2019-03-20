a06605146c4792db6a87cde06fa0282f60fc1b64
Dan Sherman <dvsherman@protonmail.com>
2019-03-20 15:30:09 -0700

    built endpoints to update and delete post by id

--- router.js
+++ router.js
@@ -52,6 +52,47 @@ router.get('/:id', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: "The post information could not be retrieved." });
   }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { title, contents } = req.body;
+
+    if (!title || !contents) {
+      return res
+        .status(400)
+        .json({ errorMessage: "Please provide title and contents for the post." });
+    } else {
+      const updatedPost = await db.update(id, req.body);
+      if (updatedPost === 0) {
+        return res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      } else {
+        res.status(200)(updatedPost);
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ error: "The post information could not be modified." });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deletedPost = await db.remove(id);
+
+    if (deletedPost === 0) {
+      return res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    } else {
+      res.status(200)
+    }
+  } catch (error) {
+    res.status(500).json({ error: "The post could not be removed" })
+  }
 })
 
 module.exports = router;
